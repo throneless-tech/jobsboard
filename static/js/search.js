824233bbd7ef88b1685ade3873edef11c4802940
rudietuesdays <rae@throneless.tech>
2020-12-21 16:15:00 -0500

    fix location filter bug

--- static/js/search.js
+++ static/js/search.js
@@ -79,10 +79,6 @@ filter.addEventListener('submit', event => {
 
   let results = miniSearch.search(options, { combineWith: "AND" });
 
-  console.log('posts: ', posts);
-  console.log('options: ', options);
-  console.log('results: ', results);
-
   if (options.length && results.length) {
     noResults.classList.add('hidden');
     postsContainer.classList.remove('hidden');
