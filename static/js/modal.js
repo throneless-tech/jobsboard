c3b77281bb0cfd64c72d4fbdc0fcffcd46073575
rudietuesdays <rae@throneless.tech>
2021-01-11 17:52:59 -0500

    build modal for community feedback info

---
+++ static/js/modal.js
@@ -0,0 +1,9 @@
+import Modal from "./modals.js";
+
+const container = document.getElementById("info");
+
+new Modal(container);
+
+// If you have more than one modal on the page
+// const containers = document.querySelectorAll(".Modal");
+// containers.forEach(container => new Modal(container));
