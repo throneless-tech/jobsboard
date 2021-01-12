94e9226c7bb2494ed3cd31dfccd3de7c3adaeeed
rudietuesdays <rae@throneless.tech>
2021-01-12 09:26:00 -0500

    add sudos

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-set -o errexit
+sudo set -o errexit
 
 sudo BASE_DIR="$(dirname "$0")"
 
 sudo apt-get update
-export DEBIAN_FRONTEND=noninteractive
+sudo export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y --no-install-recommends ca-certificates wget git hugo
 
-wget -qO- https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo wget -qO- https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Update submodule if necessary.
